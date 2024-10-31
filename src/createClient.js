109a8f39a4e7da86930a340c11030bf27a2a52a0
Daithoulung Rongmai <130676136+DisFine@users.noreply.github.com>
2024-10-31 19:00:26 +0530

    Delete src/createClient.js

--- src/createClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://tbnhzdbozhldfsjmaeap.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRibmh6ZGJvemhsZGZzam1hZWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5Mjg3NDgsImV4cCI6MjA0MzUwNDc0OH0.JS6d1fUmFP0t2lxYILbwfEOawpg0PlmIOOaL0axQKRU'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
